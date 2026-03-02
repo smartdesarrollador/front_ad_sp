81fbc6a7182c067d5c6cdd4fcf6edd6044fe0298
Jeans <sistema5000smart@gmail.com>
2026-03-02 18:05:26 -0500

    agregando docker

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,11 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.API_TARGET || 'http://localhost:8000',
         changeOrigin: true,
       },
     },
